5015ec7233fc177838f985a6378f36c0ffec409a
jhlee0934 <151573534+jhlee0934@users.noreply.github.com>
2026-09-16 17:06:32 +0900

    [Passed] 함수를 이용한 윤년 판별 (47ms, 16MB)

---
+++ trail2/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,17 @@
+y = int(input())
+
+# Please write your code here.
+
+
+def is_yunnyeon(y):
+    if y % 4 == 0:
+        if y % 100 == 0 and y % 400 != 0:
+            return 'false'
+        else:
+            return 'true'
+
+    else:
+        return 'false'
+
+
+print(is_yunnyeon(y))
