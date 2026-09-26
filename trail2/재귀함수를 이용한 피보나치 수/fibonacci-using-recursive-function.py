539933fc7749e8d7b9be9e75b49459332e21824d
jhlee0934 <151573534+jhlee0934@users.noreply.github.com>
2026-09-26 18:13:20 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (76ms, 19MB)

---
+++ trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+# Please write your code here.
+
+def fibo(N):
+    if N == 1:
+        return 1
+    
+    if N == 2:
+        return 1
+
+    return fibo(N-1) + fibo(N-2)
+
+
+print(fibo(N))
